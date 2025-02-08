58a21e3eb6f4a51ad9622ef88a57cbeac43b5b79
JaycobJuan <jparham1406@bellsouth.net>
2025-02-08 15:10:08 -0500

    base trivia game complete

--- src/main/java/com/example/triviagame/triviaGame.java
+++ src/main/java/com/example/triviagame/triviaGame.java
@@ -4,13 +4,13 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.HBox;
+import java.util.Base64;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
+import java.util.Random;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -28,8 +28,10 @@ public class triviaGame extends Application {
     private Button buttonThree;
     private Button buttonFour;
     private Button nextButton;
-    private HBox buttonsArea;
+    private VBox buttonsArea;
     private static final String API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
+    private triviaResponse tResponse;
+    private int round;
     /**
      * HTTP client.
      */
@@ -47,7 +49,7 @@ public class triviaGame extends Application {
         this.questionLabel = new Label("Loading question...");
         this.optionsBox = new VBox(10);
         this.scoreLabel = new Label("Points: 0");
-        this.buttonsArea = new HBox(20);
+        this.buttonsArea = new VBox(20);
         this.buttonOne = new Button("");
         this.buttonTwo = new Button("");
         this.buttonThree = new Button("");
@@ -65,8 +67,12 @@ public class triviaGame extends Application {
         VBox root = new VBox(20, questionLabel, optionsBox, scoreLabel, buttonsArea);
         root.setStyle("-fx-padding: 20px; -fx-alignment: center;");
         buttonsArea.setStyle("-fx-padding: 20px; -fx-alignment: center;");
+        this.buttonOne.setOnAction(e -> this.answerClicked(tResponse,round,buttonOne));
+        this.buttonTwo.setOnAction(e -> this.answerClicked(tResponse,round,buttonTwo));
+        this.buttonThree.setOnAction(e -> this.answerClicked(tResponse,round,buttonThree));
+        this.buttonFour.setOnAction(e -> this.answerClicked(tResponse,round,buttonFour));
 
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, 600, 500);
         stage.setTitle("Trivia Game");
         stage.setScene(scene);
         stage.setOnCloseRequest(event -> Platform.exit());
@@ -85,10 +91,62 @@ public class triviaGame extends Application {
             }
             String jsonString = response.body();
             System.out.println(jsonString);
-            triviaResponse tResponse = GSON.fromJson(jsonString, triviaResponse.class);
-            this.buttonOne.setText(tResponse.results[0].correct_answer);
+            tResponse = GSON.fromJson(jsonString, triviaResponse.class);
+            this.setQuestions(tResponse, 0);
         } catch (IOException | InterruptedException e) {
             System.err.println(e);
+        }
+    }
+
+    public void setQuestions(triviaResponse response, int pos) {
+        this.questionLabel.setText(response.results[pos].question);
+        Random random = new Random();
+        int num = random.nextInt(4);
+        int incNum = 0;
+        if (num == 0) {
+            this.buttonOne.setText(response.results[pos].correct_answer);
+        }
+        else {
+            this.buttonOne.setText(response.results[pos].incorrect_answers[incNum]);
+            incNum++;
+        }
+        if (num == 1) {
+            this.buttonTwo.setText(response.results[pos].correct_answer);
+        }
+        else {
+            this.buttonTwo.setText(response.results[pos].incorrect_answers[incNum]);
+            incNum++;
+        }
+        if (num == 2) {
+            this.buttonThree.setText(response.results[pos].correct_answer);
+        }
+        else {
+            this.buttonThree.setText(response.results[pos].incorrect_answers[incNum]);
+            incNum++;
+        }
+        if (num == 3) {
+            this.buttonFour.setText(response.results[pos].correct_answer);
+        }
+        else {
+            this.buttonFour.setText(response.results[pos].incorrect_answers[incNum]);
+        }
+    }
+
+    public void answerClicked(triviaResponse response, int pos, Button choice) {
+        if (choice.getText().equals(response.results[pos].correct_answer)) {
+            this.points++;
+            this.scoreLabel.setText("Points: " + points);
+        }
+        this.round++;
+           if (round < 10) {
+               this.setQuestions(tResponse, round);
+           }
+        else {
+            this.questionLabel.setText("You got " +points+ "/10 correct!");
+            this.buttonOne.setDisable(true);
+            this.buttonTwo.setDisable(true);
+            this.buttonThree.setDisable(true);
+            this.buttonFour.setDisable(true);
         }
     }
 }
